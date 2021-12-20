1666faa2bdf7a7a6bfa616656457a06f87541418
alerebal <ale.rebal@gmail.com>
2021-12-20 08:40:33 +0000

    Add credentials and conect with worksheets
    Created google sheet and conect the app with it via gspread and google auth

--- run.py
+++ run.py
@@ -1,1 +1,20 @@
-print('Hello World')
+import gspread
+from google.oauth2.service_account import Credentials
+from pprint import pprint
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+
+SH = GSPREAD_CLIENT.open('daily_menu_management')
+KIDS = SH.worksheet('kids')
+FOODS = SH.worksheet('foods')
+
+pprint(KIDS.get_all_values())
+pprint(FOODS.get_all_values())
