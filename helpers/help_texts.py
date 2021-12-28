f04fcc5ce7960f3eaf049f12f62173dfe1c349b1
alerebal <ale.rebal@gmail.com>
2021-12-28 07:41:03 +0000

    Fix help_text to be shorter than 80 col

--- helpers/help_texts.py
+++ helpers/help_texts.py
@@ -1,4 +1,5 @@
-main_menu = """
+main_menu = """Main menu
+
 Press D to daily menu.
 Press R to retrieve data.
 Press C to create data.
@@ -9,20 +10,29 @@ Press Exit to exit
 help = """
 Daily menu: 
 Choose all the children or a group of them.
-Choose a receipe from the list of recipes and check if any of them are allergic to any ingredient in the recipe.
-If no one is allergic, you can assign that recipe to the menu, otherwise, choose any recipe for allergic children from a specific list that includes all the recipes allowed for those children.
+Choose a receipe from the list of recipes and check if any of them are allergic
+to any ingredient in the recipe.
+If no one is allergic, you can assign that recipe to the menu, otherwise, 
+choose any recipe for allergic children from a specific list that includes all
+the recipes allowed for those children.
+
 Retrieve data:
-Get data about one specific child by their name or id, sorted by their group or all of the children.
+Get data about one specific child by their name or id, sorted by their group or
+all of the children.
 Get data on any recipe by name or ID, or get all recipes.
+
 Create data:
-Create data of a child following different steps. The system will guide you asking for different information about the child that is needed to create one.
+Create data of a child following different steps. The system will guide you 
+asking for different information about the child that is needed to create one.
 Create data of a recipe in the same way as to create a kid.
 """
 
 daily_menu = """
-Choose a group of children(enter BLUE, YELLOW or GREEN) or all of them(enter ALL).
+Choose a group of kids(enter BLUE, YELLOW or GREEN) or all of them(enter ALL).
 After that, choose a recipe by its ID from the list shown.
-If one or more kids are allergic to the recipe, a list of allowed recipes for each kid will be shown and you will be able to choice another recipe for those kids.
+If one or more kids are allergic to the recipe, a list of allowed recipes for 
+each kid will be shown and you will be able to choice another recipe for those 
+kids.
 """
 
 create_data = """
@@ -36,13 +46,15 @@ Press R to retrieve recipes data.
 """
 
 retrieve_users = """
-If you want to get data of one particular kid enter their name(or an approximation of it).
+If you want to get data of one particular kid enter their name(or an 
+approximation of it).
 If you want data about all the children press 'ALL'.
 Press 'BLUE', 'GREEN' or 'YELLOW' to get data of each group.
 """
 
 retrieve_recipes = """
-If you want to get a particular recipe enter its name(or an approximation of if).
+If you want to get a particular recipe enter its name(or an approximation
+of if).
 For all the recipes data press ALL.
 """
 
