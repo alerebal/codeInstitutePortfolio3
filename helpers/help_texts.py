e832d59866183b1b04e2a4e62a3ec3522529c796
alerebal <ale.rebal@gmail.com>
2022-01-08 22:10:55 +0000

    Fix helpers and help_texts to pass PEP8
    They pass now without errors

--- helpers/help_texts.py
+++ helpers/help_texts.py
@@ -1,11 +1,11 @@
 welcome = """Welcome to Daily Menu APP
 
 Create a daily menu for your primary school.
-Keep a record, create, retrieve or upload the data. 
+Keep a record, create, retrieve or upload the data.
 The data you can recording are:
 Daily menu: a main recipe for all the children or a group of them.
 In case someone were allergic to the main recipe, one recipe for they.
-Kids: Their name, age and group who they belong, tutor details and any allergy 
+Kids: Their name, age and group who they belong, tutor details and any allergy
 if they have one.
 Recipes: name and ingredients.
 """
@@ -21,11 +21,11 @@ Press Exit to exit
 
 help = """Help
 
-Daily menu: 
+Daily menu:
 Choose all the children or a group of them.
 Choose a receipe from the list of recipes and check if any of them are allergic
 to any ingredient in the recipe.
-If no one is allergic, the recipe will be assigned to the menu, otherwise, 
+If no one is allergic, the recipe will be assigned to the menu, otherwise,
 choose any recipe for allergic children from a specific list that includes all
 the recipes allowed for those children.
 The menu for all children always prevails over the groups.
@@ -36,7 +36,7 @@ all of the children.
 Get data on any recipe by name or ID, or get all recipes.
 
 Create data:
-Create data of a child following different steps. The system will guide you 
+Create data of a child following different steps. The system will guide you
 asking for different information about the child that is needed to create one.
 Create data of a recipe in the same way as to create a kid.
 """
@@ -45,8 +45,8 @@ daily_menu = """Daily menu:
 
 Choose a group of kids(enter BLUE, YELLOW or GREEN) or all of them(enter ALL).
 After that, choose a recipe by its ID from the list shown.
-If one or more kids are allergic to the recipe, a list of allowed recipes for 
-each kid will be shown and you will be able to choice another recipe for those 
+If one or more kids are allergic to the recipe, a list of allowed recipes for
+each kid will be shown and you will be able to choice another recipe for those
 kids.
 """
 
@@ -59,12 +59,12 @@ Press R to create data of a recipe.
 retrieve_data = """Retrieve data:
 
 Press K to retrieve kids data.
-Press R to retrieve recipes data. 
+Press R to retrieve recipes data.
 """
 
 retrieve_users = """Retrieve kids data:
 
-If you want to get data of one particular kid enter their name(or an 
+If you want to get data of one particular kid enter their name(or an
 approximation of it).
 If you want data about all the children press 'ALL'.
 Press 'BLUE', 'GREEN' or 'YELLOW' to get data of each group.
@@ -76,4 +76,3 @@ If you want to get a particular recipe enter its name(or an approximation
 of if).
 For all the recipes data press ALL.
 """
-
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -1,6 +1,7 @@
 import re
 import datetime
 
+
 def validate_data(inp, regex):
     """
     Check if data input have the correct format
@@ -18,34 +19,44 @@ def validate_data(inp, regex):
 
 def get_object_from_worksheet(name, worksheet):
     """
-    Get an object by their name in case that just one user exists with that name in the worksheet. Otherwise, the object id will be used. The object must be send into a list.
-    """   
+    Get an object by their name in case that just one user exists with that
+    name in the worksheet. Otherwise, the object id will be used. The object
+    must be send into a list.
+    """
     obj_list = []
     for obj in worksheet:
         if obj['name'].upper().find(name.upper()) != -1:
             obj_list.append(obj)
     if len(obj_list) < 1:
-        print('Data could not been found')
+        print('Data could not be found')
         return False
     elif len(obj_list) == 1:
         return obj_list
     else:
         print_splitter_dash()
-        print('There are more than 1 coincidence with that description. Choose one by Id\n')
+        print("""There are more than 1 coincidence with that description.
+Choose one by Id\n""")
         for obj in obj_list:
-            # If the object is a kid, can be more than one kid with the same name,so if the object has last name property show more information on screen
+            # If the object is a kid, can be more than one kid with the same
+            # name,so if the object has last name property show more
+            # information on screen
             if obj.get('last_name'):
-                print(f"Id: {obj['id']} - {obj['name']} {obj['last_name']} - Group: {obj['group']}")
+                print(f"Id: {obj['id']} - {obj['name']} {obj['last_name']}")
             else:
                 print(f"Id: {obj['id']} - {obj['name']}")
         print()
         print_splitter_dash()
-        selected_obj = False
-        while selected_obj == False:
+        # selected_obj = False
+        # while selected_obj == False:
+        obj_id = int(validate_data('Your choice:\n', '[0-9]+'))
+        selected_obj = get_data_from_id(obj_id, obj_list)
+        while not selected_obj:
+            print_splitter_dash()
+            print('Enter valid Id\n')
             obj_id = int(validate_data('Your choice:\n', '[0-9]+'))
             selected_obj = get_data_from_id(obj_id, obj_list)
         return [selected_obj]
-        
+
 
 def get_data_from_id(id, data_list):
     """
@@ -66,9 +77,9 @@ def print_splitter_dash():
 
 def print_kid(kid):
     """
-    Print a kid data with their id, name, last name and group
+    Print a kid data with their id, name and last name
     """
-    print(f"Id: {kid['id']} - {kid['name']} {kid['last_name']} - Group: {kid['group']}")
+    print(f"Id: {kid['id']} - {kid['name']} {kid['last_name']}")
 
 
 def print_kid_all_data(kid):
@@ -76,25 +87,26 @@ def print_kid_all_data(kid):
     Print all the data of a kid, without kid_id
     """
     if kid['allergies'] != '':
-        # The indentation is rere, otherwise, it put space or tabs in the print statement
+        # The indentation is rere, otherwise, it put space or tabs in the
+        # print statement
         print(f"""
-Name: {kid['name']} 
+Name: {kid['name']}
 Last name: {kid['last_name']}
 Age: {kid['age']}
 Tutor: {kid['tutor']}
 Contact: {kid['contact_number']}
 Group: {kid['group']}
-Allergies: {kid['allergies']}     
+Allergies: {kid['allergies']}
 """)
     else:
         print(f"""
-Name: {kid['name']} 
+Name: {kid['name']}
 Last name: {kid['last_name']}
 Age: {kid['age']}
 Tutor: {kid['tutor']}
 Contact: {kid['contact_number']}
 Group: {kid['group']}
-Allergies: no allergies        
+Allergies: no allergies
 """)
 
 
@@ -124,17 +136,20 @@ def print_continue_option():
 
 def find_kids_allergic_to_recipe(kids, ingredients):
     """
-    Find any kid or kids with allergies to some or several ingredients  
+    Find any kid or kids with allergies to some or several ingredients
     """
-    kids_allergic_to_recipe = []
+    kids_allergic = []
     for kid in kids:
         if not kid['allergies'] == '':
-            # I have to take off the blank spaces in both ingredients and allergies for them to match
-            kid_allergies = [aller.strip() for aller in kid['allergies'].split(',')]
+            # I have to take off the blank spaces in both ingredients and
+            # allergies for them to match
+            kid_allergies = [
+                aller.strip() for aller in kid['allergies'].split(',')
+                ]
             for ing in ingredients:
-                if ing.strip() in kid_allergies and not kid in kids_allergic_to_recipe:
-                    kids_allergic_to_recipe.append(kid)
-    return kids_allergic_to_recipe
+                if ing.strip() in kid_allergies and kid not in kids_allergic:
+                    kids_allergic.append(kid)
+    return kids_allergic
 
 
 def recipes_for_an_allergic_kid(kid, recipes):
@@ -155,20 +170,27 @@ def recipes_for_an_allergic_kid(kid, recipes):
 
 def print_menu(menus, list_of_recipes, list_of_kids):
     for menu in menus:
-        main_recipe = get_data_from_id(menu['daily_menu']['main_recipe']['recipe_id'], list_of_recipes)
+        main_recipe = get_data_from_id(
+            menu['daily_menu']['main_recipe']['recipe_id'],
+            list_of_recipes
+            )
         quantity = menu['daily_menu']['main_recipe']['quantity']
         group = menu['group'].upper()
         print_splitter_dash()
         print()
         print(f"Group: {group}\n")
-        print(f"Main recipe: {main_recipe['name']} - Rations: {quantity}\n")
+        print(f"""Main recipe: {main_recipe['name']}
+Rations: {quantity}\n""")
         if 'allergic_recipes' in menu['daily_menu'].keys():
             allergic_recipes = menu['daily_menu']['allergic_recipes']
             for recipe in allergic_recipes:
-                allergic_recipe = get_data_from_id(recipe['id'], list_of_recipes)
+                allergic_recipe = get_data_from_id(recipe['id'],
+                                                   list_of_recipes)
                 quantity = recipe['quantity']
                 kids_id = recipe['kids_id']
-                print(f"Allergic recipe: {allergic_recipe['name']} - Rations: {quantity} - For:\n")
+                print(f"""Allergic recipe: {allergic_recipe['name']}
+Rations: {quantity}
+For:""")
                 for id in kids_id:
                     kid = get_data_from_id(id, list_of_kids)
                     print(f"{kid['name']} {kid['last_name']}")
@@ -179,13 +201,16 @@ def get_date():
     """
     Get the date and give it a format month-day-number of day (Dec-Mon-23)
     """
-    date_raw = datetime.datetime.now() 
+    date_raw = datetime.datetime.now()
     return date_raw.strftime('%b-%a-%d')
 
 
 def update_menu(worksheet, date, data_to_update, group):
     """
-    Update a menu in the worksheet. Date is the string to looking for(we are looking for a specific date), data to update is what we want to modificate and group is essential because we can have more than menu for a specific date
+    Update a menu in the worksheet. Date is the string to looking for(we are
+    looking for a specific date), data to update is what we want to modificate
+    and group is essential because we can have more than menu for a specific
+    date
     """
     menus = worksheet.findall(date)
     for menu in menus:
